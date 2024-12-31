2da81084f2317540f33cf6104ffe5cbd5f7994de
jackdw258 <64153468+jackdw258@users.noreply.github.com>
2024-12-31 09:17:14 +0100

    Update and rename example.config.js to config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    client: {
+        id: '1314259205120851998' // ← Your bot ID
+    },
+    modmail: {
+        guildId: '1321557607839825920', // ← Your server ID
+        categoryId: '1321557608296873984', // ← The modmail category ID
+        staffRoles: ['1323565389745098752'], // ← The modmail staff roles IDs
+        mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
+    },
+    logs: {
+        webhookURL: 'https://discord.com/api/webhooks/1323565479721439304/KnV1FsPEhLRoYO-uyIUIvgiswvLbHVjyPra2EkccdO-8yrpO5eoUfFdzMAOPE_ZZOX8G' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+    }
+};
